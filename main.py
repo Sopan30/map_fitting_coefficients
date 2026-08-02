d988c80b53f92b2ff08bbdf9006830ab86bbbd79
Sopan30 <SOPAN.TAMBEKAR@HONEYWELL.COM>
2026-08-02 15:59:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -314,16 +314,17 @@ if file:
                             hp_coefficients = HpFitting().execute_pipeline(non_df)
                             p_coefficients = PFitting().run_calibrations(df = non_df, QrHpScaleFtr = hp_coefficients["HeadCurve"][15])
                             combine_coefficients = {
-                                "CoefficientNames": hp_coefficients["CoefficientNames"],
+                                "Variables": hp_coefficients["Variables"],
                                 "HeadCurve": hp_coefficients["HeadCurve"],
                                 "PowerCurve": p_coefficients["PowerCurve"]
                             }
                             coefficients_df = pd.DataFrame(combine_coefficients)
-                            idx_head = coefficients_df.index[df['CoefficientNames'].eq('QrMinMaxHead')][0]
-                            idx_power = coefficients_df.index[df['CoefficientNames'].eq('QrMinMaxPower')][0]
+                            idx_head = coefficients_df.index[df['Variables'].eq('QrMinMaxHead')][0]
+                            idx_power = coefficients_df.index[df['Variables'].eq('QrMinMaxPower')][0]
                             tmp = coefficients_df.at[idx_head, 'PowerCurve']
                             coefficients_df.at[idx_head, 'PowerCurve'] = coefficients_df.at[idx_power, 'HeadCurve']
                             coefficients_df.at[idx_power, 'HeadCurve'] = tmp
+                            coefficients_df.rename(columns={'Variables':'CoefficientNames'},inplace=True)
                             # st.dataframe(coefficients_df, use_container_width=True)
                             WorkbookExporter.write_stage_sheet(writer,f'{stage}_Coefficients',coefficients_df)
                             xml_content = XMLExporter.dataframe_to_tabular_xml(final_df,compressor_type,'poly')
